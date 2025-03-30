012bb303ee845858b5d025647293e74ddb123c02
chhayabangia <chhayabangia@gmail.com>
2025-03-30 05:36:51 -0400

    Updated About Me page

--- React-Portfolio/src/Pages/About.jsx
+++ React-Portfolio/src/Pages/About.jsx
@@ -2,7 +2,8 @@ export default function About() {
     return (
       <section className="about">
         <img src="/avatar.jpg" alt="Your Avatar" className="avatar" />
-        <p>Hello! I'm a full-stack web developer with experience in the MERN stack, passionate about building user-focused applications.</p>
+        <p>Hello! I’m a passionate and curious developer currently on the journey to becoming a Full-Stack Web Developer. I’m learning to build modern, responsive web applications using technologies like React, Node.js, Express, and MongoDB.
+          I enjoy solving problems, building projects that help people, and continuously improving my skills one line of code at a time. I’m excited to grow as a developer and collaborate on real-world projects that make a difference.</p>
       </section>
     );
   }
