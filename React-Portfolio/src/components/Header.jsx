63af896ada3f70f12c318442061bc618728bd089
chhayabangia <chhayabangia@gmail.com>
2025-03-30 05:08:25 -0400

    updated files

--- React-Portfolio/src/components/Header.jsx
+++ React-Portfolio/src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 export default function Header() {
   return (
     <header>
-      <h1 className="site-title">Your Name</h1>
+      <h1 className="site-title">Chhaya Bangia</h1>
       <Navigation />
     </header>
   );
