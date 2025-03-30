0603d29dc2c0b3cd86128c34d6caf549af709159
chhayabangia <chhayabangia@gmail.com>
2025-03-30 05:25:58 -0400

    Updated style and index

--- React-Portfolio/src/components/Footer.jsx
+++ React-Portfolio/src/components/Footer.jsx
@@ -2,9 +2,9 @@ export default function Footer() {
     return (
       <footer>
         <p>
-          <a href="https://github.com/yourname">GitHub</a> | 
-          <a href="https://linkedin.com/in/yourname">LinkedIn</a> | 
-          <a href="https://stackoverflow.com/users/yourid">Stack Overflow</a>
+          <a href="https://github.com/chhayabangia">GitHub</a> | 
+          <a href="https://linkedin.com/in/chhaya-bangia">LinkedIn</a> | 
+          <a href="https://stackoverflow.com/users/30109446">Stack Overflow</a>
         </p>
       </footer>
     );
